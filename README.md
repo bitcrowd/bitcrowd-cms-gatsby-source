4f39b6052df5d89fc60d5f2c90731b856ec3cb72
Malte Rohde <malte@bitcrowd.net>
2019-12-20 14:43:08 +0100

    Add page discovery and node creation.

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # TODO
+
+## Usage
+
+Add the plugin to `gatsby-config.js` as follows:
+
+```js
+plugins: [
+  {
+    resolve: `bchp6-cms-gatsby-source`,
+    options: {
+      endpoint: 'http://cms.bitcrowd.net/api/v1',
+      locale: 'int-en',
+    },
+  },
+]
+```
+
+## Development
+
+The configs can be overridden by setting environment variables.
+
+```sh
+# .env.development (if you have dotenv)
+BCHP6_CMS_GATSBY_SOURCE_ENDPOINT=http://localhost:3000/api/v1
+BCHP6_CMS_GATSBY_SOURCE_LOCALE=de-de
+```
---
+++ src/__test__/fixtures/pages-index.js
@@ -0,0 +1,43 @@
+export const empty = {
+  data: [],
+  jsonapi: {
+    version: '1.0',
+  },
+};
+
+export const one = {
+  data: [
+    {
+      id: 'cb0b940b-b1a3-49fd-bf93-3950223b29fd',
+      type: 'Page',
+      attributes: {
+        slug: 'index',
+      },
+    },
+  ],
+  jsonapi: {
+    version: '1.0',
+  },
+};
+
+export const many = {
+  data: [
+    {
+      id: 'cb0b940b-b1a3-49fd-bf93-3950223b29fd',
+      type: 'Page',
+      attributes: {
+        slug: 'index',
+      },
+    },
+    {
+      id: '4007a470-232b-11ea-aaef-0800200c9a66',
+      type: 'Page',
+      attributes: {
+        slug: 'the-cat-post',
+      },
+    },
+  ],
+  jsonapi: {
+    version: '1.0',
+  },
+};
