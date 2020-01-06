fdfe72e775c9015c14625ed2d61738353b1547bf
Malte Rohde <malte@bitcrowd.net>
2020-01-06 09:41:10 +0100

    Add page discovery and node creation. (#1)

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,10 +1,32 @@
-exports.sourceNodes = ({ actions }) => {
-  const { createNode } = actions;
+import fetch from 'node-fetch';
 
-  createNode({
-    id: '12345',
+const fetchConfig = (pluginOptions) => ({
+  endpoint: process.env.BCHP6_CMS_GATSBY_SOURCE_ENDPOINT || pluginOptions.endpoint,
+  locale: process.env.BCHP6_CMS_GATSBY_SOURCE_LOCALE || pluginOptions.locale,
+});
+
+const fetchPages = async ({ endpoint, locale }) => {
+  const pagesIndexUrl = `${endpoint}/pages/${locale}`;
+  const response = await fetch(pagesIndexUrl);
+  const { data } = await response.json();
+  return data;
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const sourceNodes = async ({ actions, createContentDigest }, pluginOptions) => {
+  const { createNode } = actions;
+  const config = fetchConfig(pluginOptions);
+
+  const pages = await fetchPages(config);
+
+  return Promise.all(pages.map((page) => createNode({
+    id: page.id,
     parent: null,
     children: [],
-    internal: { type: 'HelloWorld' },
-  });
+    internal: {
+      contentDigest: createContentDigest(page.id),
+      type: page.type,
+    },
+    slug: page.attributes.slug,
+  })));
 };
--- src/gatsby-node.test.js
+++ src/gatsby-node.test.js
@@ -1,17 +1,56 @@
+import fetch from 'node-fetch';
+
+import * as pagesIndexFixtures from './__test__/fixtures/pages-index';
+
 import { sourceNodes } from './gatsby-node';
 
+jest.mock('node-fetch', () => jest.fn());
+
+const response = (payload, status) => ({
+  ok: status >= 200 && status < 300,
+  status: status || 200,
+  json: () => payload,
+});
+
+const callSubject = async () => {
+  const actions = { createNode: jest.fn() };
+  const createContentDigest = jest.fn((x) => x);
+
+  const pluginOptions = { endpoint: 'http://example.net', locale: 'int-en' };
+
+  await sourceNodes({ actions, createContentDigest }, pluginOptions);
+
+  return actions;
+};
+
 describe('sourceNodes', () => {
-  test('creates a HelloWorld node', () => {
-    const createNode = jest.fn();
-    const actions = { createNode };
+  test('fetches published slugs from the pages#index endpoint', async () => {
+    fetch.mockResolvedValue(response(pagesIndexFixtures.empty));
 
-    sourceNodes({ actions });
+    await callSubject();
 
-    expect(createNode).toHaveBeenCalledWith({
-      id: '12345',
+    expect(fetch).toHaveBeenCalledWith('http://example.net/pages/int-en');
+  });
+
+  test('creates a node for each page', async () => {
+    fetch.mockResolvedValue(response(pagesIndexFixtures.many));
+
+    const { createNode } = await callSubject();
+
+    expect(createNode).toHaveBeenNthCalledWith(1, {
+      id: 'cb0b940b-b1a3-49fd-bf93-3950223b29fd',
       parent: null,
       children: [],
-      internal: { type: 'HelloWorld' },
+      internal: { type: 'Page', contentDigest: 'cb0b940b-b1a3-49fd-bf93-3950223b29fd' },
+      slug: 'index',
+    });
+
+    expect(createNode).toHaveBeenNthCalledWith(2, {
+      id: '4007a470-232b-11ea-aaef-0800200c9a66',
+      parent: null,
+      children: [],
+      internal: { type: 'Page', contentDigest: '4007a470-232b-11ea-aaef-0800200c9a66' },
+      slug: 'the-cat-post',
     });
   });
 });
