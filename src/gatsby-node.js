9572acca619d5e7efa284c0a5e03035b87e0a916
Malte Rohde <malte@bitcrowd.net>
2020-01-08 15:33:06 +0100

    Partially declare GraphQL schema to allow always-empty fields to be queried.

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,6 +1,45 @@
 import nodeFromResource from './node-helpers';
 import { fetchSlugs, fetchPage } from './cms-client';
 
+/*
+ * Tell Gatsby a bit about our schema, to allow it to execute
+ * queries against these fields, even if they aren't used in
+ * the CMS.
+ *
+ * Remaining fields are still inferred by Gatsby.
+ *
+ * See https://www.gatsbyjs.org/docs/schema-customization
+ * See https://github.com/gatsbyjs/gatsby/issues/2392
+ */
+export const createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+
+  const typeDefs = `
+    type CmsImageFields {
+      url: String
+    }
+
+    type CmsImage implements Node {
+      fields: CmsImageFields
+    }
+
+    type CmsRootPage implements Node {
+      seo_title_opengraph: String
+      seo_description_opengraph: String
+      seo_image_opengraph: CmsImage
+
+      seo_title_twitter: String
+      seo_description_twitter: String
+      seo_image_twitter: CmsImage
+
+      seo_title_search: String
+      seo_description_search: String
+    }
+  `;
+
+  createTypes(typeDefs);
+};
+
 export const sourceNodes = async (
   { actions, createContentDigest },
   pluginOptions
